74a493ea2db73f056ab9ca6a8be877ba97deb1ad
naofalleoagusta <naofal.leo.agusta98@gmail.com>
2022-12-08 02:48:37 +0700

    testing tag manager

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="@astrojs/image/client" />
+interface ImportMetaEnv {
+    readonly URL: string;
+    readonly GA_MEASUREMENT_ID: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
