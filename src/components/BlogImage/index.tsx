96e71fd3b7a1762248986de2b6a4de0fd5efb2ec
naofalleoagusta <naofal.leo.agusta98@gmail.com>
2023-06-29 13:42:08 +0700

    fix build

--- src/components/BlogImage/index.tsx
+++ src/components/BlogImage/index.tsx
@@ -1,5 +1,3 @@
-import { LazyLoadImage } from "react-lazy-load-image-component"
-
 type TBlogImage = {
   src: string
   alt: string
@@ -9,7 +7,7 @@ const BlogImage = ({ src, alt }: TBlogImage) => {
   return (
     <div className="flex justify-center">
       <div>
-        <LazyLoadImage src={src} alt={alt} />
+        <img src={src} alt={alt} />
         <span className="text-xs block text-gray-400 text-center mt-2">
           {alt}
         </span>
