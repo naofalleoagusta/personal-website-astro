aeded1fa440da567fe76a6d445f57a70e09f1dec
naofalleoagusta <naofal.leo.agusta98@gmail.com>
2023-07-06 04:08:21 +0700

    adjust sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,40 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import image from "@astrojs/image";
-import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
+import image from "@astrojs/image"
+import sitemap from "@astrojs/sitemap"
+import partytown from "@astrojs/partytown"
 
-import mdx from "@astrojs/mdx";
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    drafts: true
+    drafts: true,
   },
   site: "https://naofalleoagusta.xyz",
-  integrations: [tailwind(), react(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap({
-    changefreq: "weekly",
-    priority: 0.7,
-    lastmod: new Date("2022-11-27")
-  }), partytown({
-    // Adds dataLayer.push as a forwarding-event.
-    config: {
+  integrations: [
+    tailwind(),
+    react(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap({
+      changefreq: "monthly",
+      priority: 0.7,
+      lastmod: new Date("2023-07-06"),
+    }),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
       config: {
-        debug: false
+        config: {
+          debug: false,
+        },
+        forward: ["dataLayer.push"],
       },
-      forward: ["dataLayer.push"]
-    }
-  }), mdx()]
+    }),
+    mdx(),
+  ],
   // output: "server",
   // adapter: vercel()
-});
+})
