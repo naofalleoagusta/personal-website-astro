c17b875f39bf4881599ea0716b45a25986e5017c
naofalleoagusta <naofal.leo.agusta98@gmail.com>
2023-11-11 10:19:43 +0700

    fix flickering theme and bump version astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react"
-import image from "@astrojs/image"
 import sitemap from "@astrojs/sitemap"
 import partytown from "@astrojs/partytown"
-
 import mdx from "@astrojs/mdx"
+
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,9 +16,6 @@ export default defineConfig({
   integrations: [
     tailwind(),
     react(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     sitemap({
       changefreq: "monthly",
       priority: 0.7,
@@ -35,6 +32,6 @@ export default defineConfig({
     }),
     mdx(),
   ],
-  // output: "server",
-  // adapter: vercel()
+  output: "hybrid",
+  adapter: vercel(),
 })
