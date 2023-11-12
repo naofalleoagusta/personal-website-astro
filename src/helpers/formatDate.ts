f7ab42ea5c9f689c8680585c7dfbcc71341498be
naofalleoagusta <naofal.leo.agusta98@gmail.com>
2023-11-12 07:29:41 +0700

    Revert "fix date"
    This reverts commit fc94f42b0e86375f65cd9e5981c44e269f95e748.

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,19 +1,8 @@
+import { format } from "date-fns"
+
 const formatDate = (date: string, withoutHour?: boolean) => {
   const newDate = new Date(date)
-
-  const formatOptions: Intl.DateTimeFormatOptions = {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  }
-
-  if (!withoutHour) {
-    formatOptions.hour = "numeric"
-    formatOptions.minute = "numeric"
-    formatOptions.hour12 = true
-  }
-
-  return newDate.toLocaleString("en-US", formatOptions).replace(",", "")
+  return format(newDate, `${withoutHour ? "" : "hh:mm aaa |"} dd MMM yyyy`)
 }
 
 export default formatDate
